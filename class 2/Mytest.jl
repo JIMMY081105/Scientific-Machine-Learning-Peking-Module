ddf665881a4b4dfee6748949018f59bae43653a6
JIMMY081105 <hfyym4@nottingham.edu.my>
2026-06-03 13:19:58 +0800

    Add class 2 Mytest

---
+++ class 2/Mytest.jl
@@ -0,0 +1,15 @@
+x = 1
+y = 2.0
+z = "String nigga"
+
+struct House
+    Country::String
+    Size::Float32
+    Construction::String
+end
+
+MyHome = House("Inner Mongolia" , 100.0 , "Candy")
+
+vertical = [1.0, 2.0, 3.0, 4.0, 5.0, 8888.0]
+
+horizontal = [1.0 2.0 3.0 4.0 5.0 8888.0]
