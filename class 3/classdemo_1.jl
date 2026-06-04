874436f2a13da3743d92ceea47713830cfad685e
JIMMY081105 <hfyym4@nottingham.edu.my>
2026-06-04 09:36:28 +0800

    Add class 3 classdemo 1

---
+++ class 3/classdemo_1.jl
@@ -0,0 +1,22 @@
+using Optimization,OptimizationOptimisers,Zygote,CairoMakie
+
+struct Point1D
+    x::Float64
+    fvalue::Float64
+end
+
+function my_callback1D(state, l)
+    push!(vec_points,Point1D(state.u...,l))
+    return false # continues the solver
+end
+
+vec_points=[]
+
+f(x)=2*π*x[1]^2 + 2/x[1]
+
+x0=[1.8] #Define initial guess
+optf = OptimizationFunction((x,p)->f(x), ADTypes.AutoZygote()) 
+#optf = OptimizationFunction(temp, ADTypes.AutoZygote()) 
+prob = OptimizationProblem(optf, x0) #define initial guess x=1.8
+sol = solve(prob, Optimisers.Descent(0.001);maxiters=20,callback=my_callback1D)
+
