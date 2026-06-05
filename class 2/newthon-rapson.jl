a39931b614a61a89b07f99d0c807a76df31263b4
JIMMY081105 <hfyym4@nottingham.edu.my>
2026-06-05 12:22:51 +0800

    Normalize nonlinear solve demo

--- class 2/newthon-rapson.jl
+++ class 2/newthon-rapson.jl
@@ -1,9 +1,9 @@
 using NonlinearSolve
 
-f(x)=4π*x-2/x^2
+f(x) = 4pi * x - 2 / x^2
 
 
-x0=1.0 #initial guess
-p=[] #no parameters
-prob = NonlinearProblem((x,p)->f(x),x0,p)
+x0 = 1.0 # initial guess
+p = [] # no parameters
+prob = NonlinearProblem((x, p) -> f(x), x0, p)
 sol = NonlinearSolve.solve(prob)
