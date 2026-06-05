38739cbb6250053c69686ffbb2d7b4aba0099297
JIMMY081105 <hfyym4@nottingham.edu.my>
2026-06-05 13:09:34 +0800

    Refine class 3 one-dimensional optimization demo

--- class 3/classdemo_1.jl
+++ class 3/classdemo_1.jl
@@ -5,17 +5,17 @@ struct Point1D
     fvalue::Float64
 end
 
-function my_callback1D(state, l)
-    push!(vec_points,Point1D(state.u...,l))
+function my_callback1D(state, loss)
+    push!(vec_points, Point1D(state.u..., loss))
     return false # continues the solver
 end
 
-vec_points=[]
+vec_points = Point1D[]
 
-f(x)=2*π*x[1]^2 + 2/x[1]
+f(x) = 2pi * x[1]^2 + 2 / x[1]
 
 x0=[1.8] #Define initial guess
-optf = OptimizationFunction((x,p)->f(x), ADTypes.AutoZygote()) 
+optf = OptimizationFunction((x,p)->f(x), ADTypes.AutoZygote())
 #optf = OptimizationFunction(temp, ADTypes.AutoZygote()) 
 prob = OptimizationProblem(optf, x0) #define initial guess x=1.8
 sol = solve(prob, Optimisers.Descent(0.001);maxiters=20,callback=my_callback1D)
