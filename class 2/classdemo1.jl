ef85cb5723222926c5893c85dfd0b44b147df08b
JIMMY081105 <hfyym4@nottingham.edu.my>
2026-06-05 09:52:14 +0800

    Normalize class 2 area plot script

--- class 2/classdemo1.jl
+++ class 2/classdemo1.jl
@@ -1,9 +1,9 @@
 using CairoMakie
 
 
-A(r)=2π*r^2+2/r
-rplot=-0.1:0.01:2.0
+A(r) = 2pi * r^2 + 2 / r
+rplot = -0.1:0.01:2.0
 fig = Figure()
 ax = Axis(fig[1, 1], xlabel = L"r", ylabel = L"A(r)", limits = (0, 2, 0.0, 25))
-lines!(ax,rplot,A.(rplot), color =:red )
+lines!(ax, rplot, A.(rplot), color = :red)
 display(fig)
