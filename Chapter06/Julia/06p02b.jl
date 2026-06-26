3aace5146fb7e99250e480842daa0e848be59389
JIMMY081105 <182489806+JIMMY081105@users.noreply.github.com>
2026-06-26 09:37:48 +0800

    Add Chapter06/Julia/06p02b.jl

---
+++ Chapter06/Julia/06p02b.jl
@@ -0,0 +1,76 @@
+using Lux
+using Random
+using CairoMakie
+using Optimisers
+using Optimization
+using OptimizationOptimisers
+using ComponentArrays
+using Statistics
+using Zygote
+
+model01 = Chain(Dense(1 => 1))
+rng = Random.default_rng()
+parameters, layer_states = Lux.setup(rng, model01)
+
+parameters.layer_1.weight.=Float32(3.0)
+parameters.layer_1.bias.=Float32(4.0)
+
+xgrid=Array(-1:0.1:1)
+
+ygrid,_=model01(xgrid',parameters,layer_states)
+
+
+fig = Figure()
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"NN(x)")
+lines!(ax,xgrid,ygrid[:];color=:blue,label="prediction",linewidth=5)
+
+fig
+
+
+xgrid=collect(-3.0:0.01:3.0) 
+N_SAMPLES=length(xgrid)
+
+#y_prediction,_=model01(reshape(xgrid,(1,N_SAMPLES)),parameters, layer_states)
+ygrid,_=model01(xgrid',parameters, layer_states)
+#ydata=xgrid.+3+0.1*randn(N_SAMPLES)
+ydata=2.0*sin.(π*xgrid/5)+0.1*randn(N_SAMPLES)
+
+model01 = Chain(Dense(1 => 1,tanh),
+                Dense(1=>1))
+rng = Random.default_rng()
+parameters, layer_states = Lux.setup(rng, model01)
+
+function loss_fn(p, ls)
+    y_prediction, _= model01(xgrid', p, ls)
+    loss = 0.5 * mean((y_prediction[:] .- ydata).^2)
+    return loss
+end
+
+callback = function (p, l)
+    if length(loss_history) % 100 == 0
+        println("Iteration: $(p.iter), Loss: $l")
+    end
+    push!(loss_history, l)
+    return false
+end
+
+adtype = Optimization.AutoZygote()
+optf = Optimization.OptimizationFunction(loss_fn, adtype)
+optprob = Optimization.OptimizationProblem(optf, ComponentArray(parameters),layer_states)
+
+# Initialize a vector to store loss values
+loss_history = Float64[]
+neural_network = Optimization.solve(optprob, OptimizationOptimisers.Adam(0.01); callback, maxiters = 1000)
+
+optprob = Optimization.OptimizationProblem(optf, ComponentArray(neural_network.u),layer_states)
+loss_history = Float64[]
+neural_network = Optimization.solve(optprob, OptimizationOptimisers.Adam(0.01); callback, maxiters = 1000)
+
+ygrid,_=model01(xgrid',neural_network.u, layer_states)
+
+fig = Figure()
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"NN(x)")
+scatter!(ax,xgrid,ydata,color=:black)
+lines!(ax,xgrid,ygrid[:];color=:blue,label="prediction",linewidth=5)
+
+fig
