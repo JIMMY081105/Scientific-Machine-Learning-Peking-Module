3f5b0744690aa12e2e2be4a1060bbe2f3ffc7969
JIMMY081105 <182489806+JIMMY081105@users.noreply.github.com>
2026-07-12 12:20:59 +0800

    chore: sync 03p02LinearLeastSquaresNormalEquations.jl

--- Chapter03/Julia lecture/03p02LinearLeastSquaresNormalEquations.jl
+++ Chapter03/Julia lecture/03p02LinearLeastSquaresNormalEquations.jl
@@ -44,7 +44,7 @@ y_samples=3*x_samples.^2.0.+(x_samples).+0.5.+y_noise; #y=3x^2+x+0.5+epsilon
 # Let's plot the data and see how it looks like
 
 fig = Figure()
-ax = Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-1,1,0,5))
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-1,1,0,5))
 scatter!(ax,x_samples,y_samples;label="Data",color=:black,markersize=15)
 axislegend(ax;position = :lt)
 display(fig)
@@ -56,7 +56,7 @@ xplot=-1:0.01:1
 yplot=a[1].+a[2]*xplot.+a[3]*xplot.^2.0
 
 fig = Figure()
-ax = Axis(fig[1, 1], 
+ax = CairoMakie.Axis(fig[1, 1], 
     xlabel = L"x", 
     ylabel = L"y",
     xlabelsize=25,
@@ -80,7 +80,7 @@ fmodel(a,x)=a[1]*x.^0+a[2].*x.+a[3].*x.^2;
 
 fig = Figure()
 xplot=-1:0.01:1
-ax = Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-1,1,0,5))
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-1,1,0,5))
 scatter!(ax,x_samples,y_samples;label="Data",color=:black)
 lines!(ax,xplot,fmodel(a,xplot);label="Model",color=:blue)
 axislegend(ax;position = :lt)
@@ -92,5 +92,40 @@ fig
 #   domain x\in[0,2\pi]. Here, find a_0, a_1 and a_2 using the "Normal equations"
 #   method.
 
+NUMOF_SAMPLES=50 #50 data points
+#set seed to 1, so generation is consistent 
+rng=Xoshiro(1)
+
+x_samples=rand(rng,Uniform(-1,1),N_SAMPLES) #generate random values of x between -1 and 1
+y_noise=rand(rng,Normal(0.0,0.1),N_SAMPLES) #generate some noise, normal distribution with standard deviation of 0.1
+y_samples=2*x_samples.^2.0.+10*(x_samples).+3.0.+y_noise;
+
+fig = Figure()
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-3,3,-6,25))
+scatter!(ax,x_samples,y_samples;label="Data",color=:black,markersize=15)
+axislegend(ax;position = :lt)
+display(fig)
+
+a=[1.0,1.0,1.0] #initial assumptions of the values of  a0, a1, a2.  Remember that the indexing in Julia
+                # starts from 1, not 0. So a[1] is a0, a[2] is a1 and a[3] is a2.
+xplot=-1:0.01:1
+yplot=a[1].+a[2]*xplot.+a[3]*xplot.^2.0
+
+V=hcat(ones(N_SAMPLES),x_samples,x_samples.^2)
+a=V'V\(V'y_samples)
+# Note that the solution you have found is
+
+fmodel(a,x)=a[1]*x.^0+a[2].*x.+a[3].*x.^2;
+
+fig = Figure()
+xplot=-1:0.01:1
+ax = CairoMakie.Axis(fig[1, 1], xlabel = L"x", ylabel = L"y",limits=(-1,1,0,5))
+scatter!(ax,x_samples,y_samples;label="Data",color=:black)
+lines!(ax,xplot,fmodel(a,xplot);label="Model",color=:blue)
+axislegend(ax;position = :lt)
+fig
+
 # TODO (Class Demo 02): Read the data from the file "Data.csv" and try to see if you can get a good fit
 #   with a third order polynomial
+
+
